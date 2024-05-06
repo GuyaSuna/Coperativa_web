87125b8d632226be40c58a5679e8f8853ff6b3a5
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2024-05-06 10:28:52 -0300

    Api update

--- src/app/Api/api.js
+++ src/app/Api/api.js
@@ -10,7 +10,7 @@ const getSocio = async (UserNumber) => {
         });
 
         if (!response.ok) {
-            throw new Error("Te jodiste");
+            throw new Error("The peticion has failed, response is not ok");
         }
 
         const data = await response.json();
@@ -22,6 +22,32 @@ const getSocio = async (UserNumber) => {
     }
 };
 
+const login = async (adminName , adminPassword) => {
+    try {
+       const body = {
+            adminName : adminName,
+            adminPassword : adminPassword, 
+        }
+        const response = await fetch(`${URL}/login}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        });
+
+        if (!response.ok) {
+            throw new Error("Te jodiste");
+        }
+
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        console.error("Error en getSocio:", error);
+        throw new Error("Error al obtener los datos del socio");
+    }
+};
 
 
 
