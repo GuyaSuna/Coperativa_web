4c732885f837e1a07fd994eb85c29f6393ae8cf2
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2024-07-12 10:17:25 -0300

    Login conectado con api

---
+++ src/app/UsuarioHome/page.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+import logo from "../../../public/LogoApp.jpg";
+import Image from "next/image";
+
+export default function AdminHome() {
+  return (
+
+    <div>
+        hola
+    </div>
+   )
+
+}
