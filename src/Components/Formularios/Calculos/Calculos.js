2f32a99dab52461caf36b5f65fd47c9da30bc6e5
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2024-08-07 11:36:11 -0300

    calculos excenReader

---
+++ src/Components/Formularios/Calculos/Calculos.js
@@ -0,0 +1,38 @@
+// components/ExcelReader.js
+import React, { useState } from 'react';
+import * as XLSX from 'xlsx';
+
+const Calculos = () => {
+  const [data, setData] = useState([]);
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const binaryStr = e.target.result;
+      const workbook = XLSX.read(binaryStr, { type: 'binary' });
+
+      // Aquí asumimos que los datos están en la primera hoja
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const jsonData = XLSX.utils.sheet_to_json(worksheet);
+
+      setData(jsonData);
+    };
+
+    reader.readAsBinaryString(file);
+  };
+
+  return (
+    <div>
+      <input type="file" accept=".xlsx, .xls" onChange={handleFileUpload} />
+      <div>
+        <h2>Datos del archivo:</h2>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+    </div>
+  );
+};
+
+export default Calculos;
