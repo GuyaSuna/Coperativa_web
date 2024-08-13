5202b4217007ebc9e67f7660b61569306ec179d4
AlexSumner <111889133+AlexSumner@users.noreply.github.com>
2024-08-12 22:24:39 -0300

    modales estilos andando ok

--- src/Components/VerDetalles/VerSuplente/VerSuplente.js
+++ src/Components/VerDetalles/VerSuplente/VerSuplente.js
@@ -9,7 +9,7 @@ const VerSuplente = ({ suplente, socio, isOpen, onClose }) => {
     <>
       <div className="fixed inset-0 bg-gray-600 bg-opacity-50 z-50"></div>
       <div className="fixed inset-0 flex items-center justify-center z-50">
-        <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg w-11/12 md:w-1/3 p-4">
+        <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg w-11/12 md:w-1/2 lg:w-1/3 p-6">
           <button
             onClick={onClose}
             className="absolute top-3 right-3 text-gray-400 dark:text-gray-600 hover:text-gray-800 dark:hover:text-gray-200"
@@ -26,58 +26,49 @@ const VerSuplente = ({ suplente, socio, isOpen, onClose }) => {
               <path d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
-          <div className="mx-auto">
-            <table className="w-full text-left">
-              <thead>
-                <tr className="text-gray-400">
-                  <th className="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800">
-                    Cedula
-                  </th>
-                  <th className="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800">
-                    Nombre
-                  </th>
-                  <th className="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800">
-                    Apellido
-                  </th>
-                  <th className="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800">
-                    Telefono
-                  </th>
-                  <th className="font-normal px-3 pt-0 pb-3 border-b border-gray-200 dark:border-gray-800">
-                    Socio
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="text-gray-600 dark:text-gray-100">
-                <tr>
-                  <td className="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800">
-                    <div className="flex items-center">
-                      {suplente.cedulaSuplente}
-                    </div>
-                  </td>
-                  <td className="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800">
-                    {suplente.nombreSuplente}
-                  </td>
-                  <td className="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800">
-                    {suplente.apellidoSuplente}
-                  </td>
-                  <td className="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800">
-                    {suplente.telefonoSuplente}
-                  </td>
-                  <td className="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800">
-                    <div className="flex items-center">
-                      {socio
-                        ? `${socio.nombreSocio} ${socio.apellidoSocio}`
-                        : "No disponible"}
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+          <div className="p-6 space-y-4">
+            <h3 className="text-2xl font-semibold text-white-900 mb-4">
+              Datos del Suplente
+            </h3>
+            <div className="flex flex-col space-y-2">
+              <div className="flex justify-between">
+                <span className="font-medium text-white-800">Cédula:</span>
+                <span className="text-white-600">
+                  {suplente.cedulaSuplente}
+                </span>
+              </div>
+              <div className="flex justify-between">
+                <span className="font-medium text-white-800">Nombre:</span>
+                <span className="text-white-600">
+                  {suplente.nombreSuplente}
+                </span>
+              </div>
+              <div className="flex justify-between">
+                <span className="font-medium text-white-800">Apellido:</span>
+                <span className="text-white-600">
+                  {suplente.apellidoSuplente}
+                </span>
+              </div>
+              <div className="flex justify-between">
+                <span className="font-medium text-white-800">Teléfono:</span>
+                <span className="text-white-600">
+                  {suplente.telefonoSuplente}
+                </span>
+              </div>
+              <div className="flex justify-between">
+                <span className="font-medium text-white-800">Socio:</span>
+                <span className="text-white-600">
+                  {socio
+                    ? `${socio.nombreSocio} ${socio.apellidoSocio}`
+                    : "No disponible"}
+                </span>
+              </div>
+            </div>
           </div>
-          <div className="mt-8 flex justify-center">
+          <div className="flex justify-center mt-6">
             <button
               type="button"
-              className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-3 py-1 text-xs font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-all duration-150 ease-in-out"
+              className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-all duration-150 ease-in-out"
               onClick={onClose}
             >
               Cerrar
