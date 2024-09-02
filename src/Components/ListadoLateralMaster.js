a0a0b031a31137b513c2ae8471338c45f907bcd7
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2024-09-02 10:09:23 -0300

    detalles

--- src/Components/ListadoLateralMaster.js
+++ src/Components/ListadoLateralMaster.js
@@ -21,6 +21,14 @@ const ListadoLateralMaster = ({ setIdentificadorComponente }) => {
         >
           Agregar Cooperativas
         </button>
+        <button
+          onClick={() => handleSelection(28)}
+          className={`w-full py-2 px-4 rounded-md text-left font-semibold dark:text-white text-black hover:bg-blue-100 dark:hover:bg-blue-900 ${
+            selectedOption === 28 ? "bg-blue-500 text-white" : ""
+          }`}
+        >
+          Agregar Interes-Capital
+        </button>
       </div>
       <div className="space-y-4 mt-3">
       </div>
