96b644c465e766c4a556b7207fe6ad689ae5955d
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2024-09-15 20:23:21 -0300

    Preparate porque te voy a reventar B)

--- src/Components/Listados/ListadoSocios/ListadoSocio.js
+++ src/Components/Listados/ListadoSocios/ListadoSocio.js
@@ -156,6 +156,11 @@ const ListadoSocio = ({
     },
   ];
 
+  const handleCrearRecibo = (socio) =>{
+    setSocioRecibo(socio) 
+    setIdentificadorComponente(6)
+  }
+
   const handleSortChange = (option) => {
     console.log("Orden seleccionado:", option.label);
     if (buscador) {
