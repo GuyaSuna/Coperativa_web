71736a9ecd437a0a36baf76fd3a38fe14882e6c6
AleSpinelli <alejospinelli2@gmail.com>
2024-09-26 12:02:21 -0300

    Ok

--- src/Components/UsuarioDashboard/ListadoRecibosSocio.js
+++ src/Components/UsuarioDashboard/ListadoRecibosSocio.js
@@ -17,7 +17,7 @@ import {
 } from "@headlessui/react";
 import IconButton from "@mui/material/IconButton";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import VerRecibo from "../VerDetalles/VerRecibo/VerRecibo";
+import VerRecibo from "../VerDetalles/VerRecibo/verRecibo";
 import DashboardCard from "./DashboardCard";
 import { getAllRecibosPorSocio } from "@/Api/api";
 import { MiembroContext } from "@/Provider/provider";
@@ -58,7 +58,7 @@ const ListadoRecibosSocios = () => {
   const handleVerRecibo = (recibo) => {
     setReciboSeleccionado(recibo);
     setIsModalOpen(true);
-  }
+  };
   const handleDescargarPDF = (recibo) => {
     const doc = new jsPDF();
 
@@ -108,182 +108,182 @@ const ListadoRecibosSocios = () => {
     doc.save(`Recibo_${recibo.fechaRecibo}.pdf`);
   };
   return (
-  <>
-    <DashboardCard title={"Historial de Recibos"}>
-      <Box
-        sx={{
-          overflowX: "auto", // Allows horizontal scrolling if content overflows
-          width: "100%", // Ensure the box takes full width
-          minWidth: "100%",
-        }}
-      >
-        <Table
-          aria-label="simple table"
+    <>
+      <DashboardCard title={"Historial de Recibos"}>
+        <Box
           sx={{
-            whiteSpace: "nowrap", // Prevent text wrapping
-            mt: 2,
-            minWidth: { xs: "500px", sm: "650px" }, // Set a minimum width for the table
+            overflowX: "auto", // Allows horizontal scrolling if content overflows
+            width: "100%", // Ensure the box takes full width
+            minWidth: "100%",
           }}
-          className="dark:bg-gray-100 bg-dark text-white dark:text-gray-600 "
         >
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                <Typography
-                  variant="subtitle2"
-                  fontWeight={600}
-                  className="dark:bg-gray-100 bg-dark text-white dark:text-gray-600 "
-                >
-                  Nro Recibo
-                </Typography>
-              </TableCell>
-              <TableCell>
-                <Typography
-                  variant="subtitle2"
-                  fontWeight={600}
-                  className="dark:bg-gray-100 bg-dark text-white dark:text-gray-600 "
-                >
-                  Fecha Emision
-                </Typography>
-              </TableCell>
-              <TableCell>
-                <Typography
-                  variant="subtitle2"
-                  fontWeight={600}
-                  className="dark:bg-gray-100 bg-dark text-white dark:text-gray-600 "
-                >
-                  Fecha del Pago
-                </Typography>
-              </TableCell>
-              <TableCell>
-                <Typography
-                  variant="subtitle2"
-                  fontWeight={600}
-                  className="dark:bg-gray-100 bg-dark text-white dark:text-gray-600 "
-                >
-                  Valor Cuota
-                </Typography>
-              </TableCell>
-              <TableCell align="right">
-                <Typography
-                  variant="subtitle2"
-                  fontWeight={600}
-                  className="dark:bg-gray-100 bg-dark text-white dark:text-gray-600 "
-                ></Typography>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {allRecibos.map((recibo) => (
+          <Table
+            aria-label="simple table"
+            sx={{
+              whiteSpace: "nowrap", // Prevent text wrapping
+              mt: 2,
+              minWidth: { xs: "500px", sm: "650px" }, // Set a minimum width for the table
+            }}
+            className="dark:bg-gray-100 bg-dark text-white dark:text-gray-600 "
+          >
+            <TableHead>
               <TableRow>
                 <TableCell>
                   <Typography
-                    sx={{
-                      fontSize: "15px",
-                      fontWeight: "500",
-                    }}
+                    variant="subtitle2"
+                    fontWeight={600}
                     className="dark:bg-gray-100 bg-dark text-white dark:text-gray-600 "
                   >
-                    {recibo.nroRecibo}
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography
-                    sx={{
-                      display: "flex",
-                      alignItems: "center",
-                    }}
-                    className="dark:bg-gray-100 bg-dark text-white dark:text-gray-600 "
-                  >
-                    {recibo.fechaRecibo}
+                    Nro Recibo
                   </Typography>
                 </TableCell>
                 <TableCell>
                   <Typography
                     variant="subtitle2"
-                    fontWeight={400}
+                    fontWeight={600}
                     className="dark:bg-gray-100 bg-dark text-white dark:text-gray-600 "
                   >
-                    {recibo.fechaPago}
+                    Fecha Emision
                   </Typography>
                 </TableCell>
                 <TableCell>
                   <Typography
                     variant="subtitle2"
-                    fontWeight={400}
+                    fontWeight={600}
                     className="dark:bg-gray-100 bg-dark text-white dark:text-gray-600 "
                   >
-                    ${recibo.cuotaMensual}
+                    Fecha del Pago
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography
+                    variant="subtitle2"
+                    fontWeight={600}
+                    className="dark:bg-gray-100 bg-dark text-white dark:text-gray-600 "
+                  >
+                    Valor Cuota
                   </Typography>
                 </TableCell>
                 <TableCell align="right">
-                  <Menu
-                    as="div"
-                    className="relative inline-block text-left justify-end"
-                  >
-                    <div>
-                      <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold dark:bg-gray-100 bg-dark text-white dark:text-gray-600 shadow-sm">
-                        <svg
-                          viewBox="0 0 24 24"
-                          className="w-5"
-                          stroke="currentColor"
-                          strokeWidth={2}
-                          fill="none"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        >
-                          <circle cx={12} cy={12} r={1} />
-                          <circle cx={19} cy={12} r={1} />
-                          <circle cx={5} cy={12} r={1} />
-                        </svg>
-                      </MenuButton>
-                    </div>
-                    <MenuItems
-                      transition
-                      className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none"
-                    >
-                      <div className="py-1">
-                        <MenuItem>
-                          <button
-                            onClick={() => handleEliminar(recibo?.idRecibo)}
-                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                          >
-                            Imprimir
-                          </button>
-                        </MenuItem>
-                        <MenuItem>
-                          <button
-                            onClick={() => handleVerRecibo(recibo)}
-                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                          >
-                            Ver Recibo
-                          </button>
-                        </MenuItem>
-                        <MenuItem>
-                          <button
-                            onClick={() => handleDescargarPDF(recibo)}
-                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                          >
-                            Descargar PDF
-                          </button>
-                        </MenuItem>
-                      </div>
-                    </MenuItems>
-                  </Menu>
+                  <Typography
+                    variant="subtitle2"
+                    fontWeight={600}
+                    className="dark:bg-gray-100 bg-dark text-white dark:text-gray-600 "
+                  ></Typography>
                 </TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Box>
-    </DashboardCard>
-     {isModalOpen && (
-      <VerRecibo
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        recibo={reciboSeleccionado}
-      />
-    )}
+            </TableHead>
+            <TableBody>
+              {allRecibos.map((recibo) => (
+                <TableRow>
+                  <TableCell>
+                    <Typography
+                      sx={{
+                        fontSize: "15px",
+                        fontWeight: "500",
+                      }}
+                      className="dark:bg-gray-100 bg-dark text-white dark:text-gray-600 "
+                    >
+                      {recibo.nroRecibo}
+                    </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography
+                      sx={{
+                        display: "flex",
+                        alignItems: "center",
+                      }}
+                      className="dark:bg-gray-100 bg-dark text-white dark:text-gray-600 "
+                    >
+                      {recibo.fechaRecibo}
+                    </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography
+                      variant="subtitle2"
+                      fontWeight={400}
+                      className="dark:bg-gray-100 bg-dark text-white dark:text-gray-600 "
+                    >
+                      {recibo.fechaPago}
+                    </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Typography
+                      variant="subtitle2"
+                      fontWeight={400}
+                      className="dark:bg-gray-100 bg-dark text-white dark:text-gray-600 "
+                    >
+                      ${recibo.cuotaMensual}
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="right">
+                    <Menu
+                      as="div"
+                      className="relative inline-block text-left justify-end"
+                    >
+                      <div>
+                        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold dark:bg-gray-100 bg-dark text-white dark:text-gray-600 shadow-sm">
+                          <svg
+                            viewBox="0 0 24 24"
+                            className="w-5"
+                            stroke="currentColor"
+                            strokeWidth={2}
+                            fill="none"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          >
+                            <circle cx={12} cy={12} r={1} />
+                            <circle cx={19} cy={12} r={1} />
+                            <circle cx={5} cy={12} r={1} />
+                          </svg>
+                        </MenuButton>
+                      </div>
+                      <MenuItems
+                        transition
+                        className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none"
+                      >
+                        <div className="py-1">
+                          <MenuItem>
+                            <button
+                              onClick={() => handleEliminar(recibo?.idRecibo)}
+                              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+                            >
+                              Imprimir
+                            </button>
+                          </MenuItem>
+                          <MenuItem>
+                            <button
+                              onClick={() => handleVerRecibo(recibo)}
+                              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+                            >
+                              Ver Recibo
+                            </button>
+                          </MenuItem>
+                          <MenuItem>
+                            <button
+                              onClick={() => handleDescargarPDF(recibo)}
+                              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+                            >
+                              Descargar PDF
+                            </button>
+                          </MenuItem>
+                        </div>
+                      </MenuItems>
+                    </Menu>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Box>
+      </DashboardCard>
+      {isModalOpen && (
+        <VerRecibo
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          recibo={reciboSeleccionado}
+        />
+      )}
     </>
   );
 };
