788e29b475c640270039f1f967a1cb45ec8f366c
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2024-10-10 18:48:33 -0300

    pdf balance

--- src/Components/Listados/ListadoBalanceAnual/listadoBalanceAnual.js
+++ src/Components/Listados/ListadoBalanceAnual/listadoBalanceAnual.js
@@ -5,7 +5,14 @@ import { MiembroContext } from "@/Provider/provider.js";
 import Buscador from "@/Components/Buscador.js";
 import OrdenarPor from "@/Components/OrdenarPor.js";
 import SortIcon from "@mui/icons-material/Sort";
-
+import { jsPDF } from "jspdf";
+import {
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/react";
 const ListadoBalanceAnual = () => {
   const [balances, setBalances] = useState([]);
   const [buscador, setBuscador] = useState("");
@@ -62,6 +69,69 @@ const ListadoBalanceAnual = () => {
     setBalances(balancesOrdenados);
   };
 
+  const descargarPdfBalanceAnual = (ultimoBalance) => {
+    console.log("ULTIMO", ultimoBalance);
+  
+    if (!ultimoBalance) {
+      alert("No se encontraron datos de balance anual para generar el PDF.");
+      return;
+    }
+  
+    const { cooperativaEntity, egresoTotal, ingresoTotal, resultadoFinal, listaEgresos, listaIngresos, fechaBalanceAnual } = ultimoBalance;
+  
+    const fechaBalance = new Date(fechaBalanceAnual + "T00:00:00");
+    const doc = new jsPDF();
+  
+    doc.setFontSize(18);
+    doc.text(`Balance Anual - ${cooperativaEntity.nombre}`, 14, 22);
+  
+    doc.setFontSize(12);
+    doc.text(`Año: ${fechaBalance.getFullYear()}`, 170, 22, { align: 'right' });
+    
+  
+
+    const ingresosStartY = 40;
+    doc.setFontSize(14);
+    doc.text("Ingresos:", 14, ingresosStartY);
+  
+    doc.autoTable({
+      head: [["SubRubro", "Monto"]],
+      body: listaIngresos.length > 0 
+        ? listaIngresos.map(ingreso => [ingreso.subRubro, ingreso.ingreso?.toFixed(2)]) 
+        : [["Sin ingresos", "0.00"]],
+      startY: ingresosStartY + 5, 
+      theme: "grid",
+      
+    });
+  
+
+    const egresosStartY = doc.autoTable.previous.finalY + 10;
+    doc.setFontSize(14);
+    doc.text("Egresos:", 14, egresosStartY);
+  
+
+    doc.autoTable({
+      head: [["SubRubro", "Monto"]],
+      body: listaEgresos.length > 0 
+        ? listaEgresos.map(egreso => [egreso.subRubro, egreso.egreso?.toFixed(2)]) 
+        : [["Sin egresos", "0.00"]],
+      startY: egresosStartY + 5,
+      theme: "grid",
+    });
+  
+    // Totales y resultado final
+    const totalsStartY = doc.autoTable.previous.finalY + 10;
+    doc.setFontSize(12);
+    doc.text(
+      `Total Ingresos: ${ingresoTotal?.toFixed(2)} - Total Egresos: ${egresoTotal?.toFixed(2)} - Resultado: ${resultadoFinal.toFixed(2)}`,
+      14,
+      totalsStartY
+    );
+  
+    // Guarda el archivo PDF con el nombre adecuado
+    doc.save(`Balance_Anual_${fechaBalance.getFullYear()}.pdf`);
+  };
+
   return (
     <div className="sm:p-7 p-4">
       <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
@@ -108,9 +178,48 @@ const ListadoBalanceAnual = () => {
                 <td className="block sm:table-cell px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                   ${balance.resultadoFinal}
                 </td>
+                <td className="block sm:table-cell px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"></td>
+                <div className="relative inline-block text-left">
+                    <Menu as="div" className="relative inline-block text-left">
+                      <MenuButton className="focus:outline-none font-medium rounded-lg text-sm px-2 py-2 text-center inline-flex items-center hidden md:inline-flex">
+                        <svg
+                          viewBox="0 0 24 24"
+                          className="w-5"
+                          stroke="currentColor"
+                          strokeWidth={2}
+                          fill="none"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <circle cx={12} cy={12} r={1} />
+                          <circle cx={19} cy={12} r={1} />
+                          <circle cx={5} cy={12} r={1} />
+                        </svg>
+                      </MenuButton>
+                      <MenuItems
+                        transition
+                        className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none"
+                      >
+                        <div className="py-1">
+                          <MenuItem>
+                            <button
+                              onClick={() =>
+                                descargarPdfBalanceAnual(balance)
+                              }
+                              className="block px-4 py-2 text-sm text-gray-700 focus:bg-gray-100 focus:text-gray-900"
+                            >
+                              Descargar Pdf
+                            </button>
+                          </MenuItem>
+                        </div>
+                      </MenuItems>
+                    </Menu>
+                  </div>
+                  
               </tr>
             ))}
           </tbody>
+        
         </table>
       </div>
     </div>
